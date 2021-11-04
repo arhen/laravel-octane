49060587cf5fd526d61ce35a19ede776046f6fe0
Arhen <itsme.arhen@gmail.com>
2021-11-05 03:15:42 +0900

    add GET /fibonacci to test perf

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// create route function for return json list of fibonacci 100 numbers serve at GET /fibonacci
+Route::get('/fibonacci', function () {
+    $fibonacci = [];
+    $fibonacci[0] = 0;
+    $fibonacci[1] = 1;
+    for ($i = 2; $i < 10; $i++) {
+        $fibonacci[$i] = $fibonacci[$i - 1] + $fibonacci[$i - 2];
+    }
+    return response()->json($fibonacci);
+});
